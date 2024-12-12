f3c78a2bac7f17f0580d5cc6839555832a22039c
Emiliano Rodriguez <emilianor81@gmail.com>
2024-12-12 17:30:49 +0100

    Add footer on home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,24 +6,30 @@ import VideoGallery from './components/VideoGallery';
 import Sponsors from './components/Sponsors';
 import Contact from './components/Contact';
 import AdminPanel from './components/admin/AdminPanel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Navigation />
-            <Hero />
-            <About />
-            <VideoGallery />
-            <Sponsors />
-            <Contact />
-          </>
-        } />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Navigation />
+                <Hero />
+                <About />
+                <VideoGallery />
+                <Sponsors />
+                <Contact />
+              </>
+            } />
+            <Route path="/admin" element={<AdminPanel />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
