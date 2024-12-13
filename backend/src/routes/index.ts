7a941bd3c8d68df3f5c79beeafa11327f8ee9edc
Emiliano Rodriguez <emilianor81@gmail.com>
2024-12-13 10:33:32 +0100

    Add new auth routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import videoRoutes from './videos';
+import authRoutes from './auth';
 
 const router = Router();
 
 // Aquí puedes agregar más rutas en el futuro
 router.use('/videos', videoRoutes);
+router.use('/auth', authRoutes);
 
 export default router; 
