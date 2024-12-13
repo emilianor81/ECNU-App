8e65695b4339cb7501af75c50f859f145249df21
Emiliano Rodriguez <emilianor81@gmail.com>
2024-12-13 09:45:49 +0100

    Add middleware verify token to videos routes

--- backend/src/routes/videos.ts
+++ backend/src/routes/videos.ts
@@ -5,12 +5,13 @@ import {
   updateVideo, 
   deleteVideo 
 } from '../controllers/videos.controller';
+import { verifyToken } from '../middlewares/auth';
 
 const router = Router();
 
 router.get('/', getVideos as RequestHandler);
-router.post('/', createVideo as RequestHandler);
-router.put('/:id', updateVideo as RequestHandler);
-router.delete('/:id', deleteVideo as RequestHandler);
+router.post('/', verifyToken, createVideo as RequestHandler);
+router.put('/:id', verifyToken, updateVideo as RequestHandler);
+router.delete('/:id', verifyToken, deleteVideo as RequestHandler);
 
 export default router; 
